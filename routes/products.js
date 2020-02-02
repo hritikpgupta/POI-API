a5b0dfe95a92670cf16b2483d234986c48975a8c
Hriitk <hritikgupta722@gmail.com>
2020-02-02 22:58:07 +0530

    Completed Product Routes
    Added get and post end points for products.Next task is to work on User end points.

---
+++ routes/products.js
@@ -0,0 +1,37 @@
+const { Products, Variant, validate } = require('../models/product');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let product = new Products({
+        uniqueID: req.body.uniqueID,
+        productName: req.body.productName,
+        variants: req.body.variants,
+        description: req.body.description,
+        generalUrl: req.body.generalUrl
+
+    })
+    product = await product.save()
+
+    res.send(product)
+
+})
+
+
+router.get('/', async (req, res) => {
+
+    const result = await Products.find()
+    res.send(result)
+
+
+})
+
+
+
+
+
+
+module.exports = router; 
