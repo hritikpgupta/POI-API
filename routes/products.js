21f1eb135a0da6aafd14270c9b69ac781399c910
Hriitk <hritikgupta722@gmail.com>
2020-02-03 20:28:45 +0530

    Fixed Product End-point

--- routes/products.js
+++ routes/products.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
 })
 
 
-router.get('/getUser/:id', async (req, res) => {
+router.get('/', async (req, res) => {
 
     const result = await Products.findOne({ mobileNumber: { $eq: parseInt(req.params.id) } })
     res.send(result)
